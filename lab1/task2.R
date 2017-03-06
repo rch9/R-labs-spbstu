fc616ae3631342118e2afe8a073282749efcbbb3
rch_9 <enlofpoliteh121@gmail.com>
2017-03-06 15:04:49 +0300

    added favtors

--- lab1/task2.R
+++ lab1/task2.R
@@ -1,8 +1,10 @@
+library(e1071)
+
 # train
 
 X1 = c(rnorm(50, mean = 10, sd = 4), rnorm(50, mean = 20, sd = 3))
 X2 = c(rnorm(50, mean = 14, sd = 4), rnorm(50, mean = 18, sd = 3))
-TYPE = c(rep.int(1, 50), rep.int(2, 50))
+TYPE = c(rep.int("1", 50), rep.int("2", 50))
 
 f = data.frame(X1, X2, TYPE)
 
@@ -12,16 +14,23 @@ classifier <- naiveBayes(f[,-3], f$TYPE)
 
 
 # test
+set.seed(64761)
 
 X1 = c(rnorm(50, mean = 10, sd = 4), rnorm(50, mean = 20, sd = 3))
 X2 = c(rnorm(50, mean = 14, sd = 4), rnorm(50, mean = 18, sd = 3))
-TYPE = c(rep.int(1, 50), rep.int(2, 50))
+TYPE = c(rep.int("1", 50), rep.int("2", 50))
+
+
 
 f_test = data.frame(X1, X2, TYPE)
 
 print(f_test)
 
-predicted.d = predict(classifier, f_test[,-3]) 
+predictedd = predict(classifier, f_test[,-3]) 
 
-table(predicted.d) 
+print(classifier)
+
+table(predictedd) 
 table(f_test$TYPE)
+
+table(predictedd, f_test$TYPE)
