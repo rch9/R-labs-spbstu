147d4571c0a63475463fb4101c7731caac03439a
rch9 <enlofpoliteh121@gmail.com>
2017-03-06 09:23:16 +0300

    added test

--- lab1/task2.R
+++ lab1/task2.R
@@ -1,7 +1,27 @@
+# train
+
 X1 = c(rnorm(50, mean = 10, sd = 4), rnorm(50, mean = 20, sd = 3))
 X2 = c(rnorm(50, mean = 14, sd = 4), rnorm(50, mean = 18, sd = 3))
 TYPE = c(rep.int(1, 50), rep.int(2, 50))
 
 f = data.frame(X1, X2, TYPE)
 
-pairs(f[1:2], main = "i am god")
+pairs(f[1:2], main = "2types", pch = 21, bg=c("red", "blue")[unclass(f$TYPE)])
+
+classifier <- naiveBayes(f[,-3], f$TYPE)
+
+
+# test
+
+X1 = c(rnorm(50, mean = 10, sd = 4), rnorm(50, mean = 20, sd = 3))
+X2 = c(rnorm(50, mean = 14, sd = 4), rnorm(50, mean = 18, sd = 3))
+TYPE = c(rep.int(1, 50), rep.int(2, 50))
+
+f_test = data.frame(X1, X2, TYPE)
+
+print(f_test)
+
+predicted.d = predict(classifier, f_test[,-3]) 
+
+table(predicted.d) 
+table(f_test$TYPE)
