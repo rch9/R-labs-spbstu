0da460aba23da88c28c3f904709987bd73f8c432
rch9 <enlofpoliteh121@gmail.com>
2017-03-20 23:06:42 +0300

    fix example 1

---
+++ lab2/example2.R
@@ -0,0 +1,24 @@
+# install.packages("mlbench")
+# library(mlbench)
+library(kknn)
+## загружаем данные Ionospere 
+data(ionosphere)
+## смотрим количество примеров и признаков в ionosphere
+dim(ionosphere)
+## смотрим заголовки признаков и классов
+ionosphere[0,]
+## смотрим метки классов всех примеров
+ionosphere[,"class"]
+## выбираем первые 200 примеров для обучения
+ionosphere.learn <- ionosphere[1:200,]
+## оставшиеся примеры используем для тестирования
+ionosphere.valid <- ionosphere[-c(1:200),]
+## запускаем процедуру kknn
+fit.kknn <- kknn(class ~ ., ionosphere.learn, ionosphere.valid)
+## запускаем процедуру train.kknn, используя 
+## расстояние Минковского с параметром 1 и с различными ядрами, максимальное K для поиска оптимального K равно 15
+fit.train1 <- train.kknn(class ~ ., ionosphere.learn, kmax = 15,kernel = c("triangular", "rectangular", "epanechnikov", "optimal"), distance = 1)
+fit.train2 <- train.kknn(class ~ ., ionosphere.learn, kmax = 15, kernel = c("triangular", "rectangular", "epanechnikov", "optimal"), distance = 2)
+plot(fit.train1)
+plot(fit.train2)
+
