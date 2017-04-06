c35942d9c3ff9c621db317db4c8e86a9d062083d
rch9 <enlofpoliteh121@gmail.com>
2017-04-06 14:06:12 +0300

    add task1

--- lab2/task1_email.R
+++ lab2/task1_email.R
@@ -1,9 +1,33 @@
-install.packages("kernlab")
+# Исследуйте, как объем обучающей выборки и количество тестовых данных, 
+# влияет на точность классификации или на вероятность ошибочной 
+# классификации в примере крестики-нолики и примере о спаме e-mail сообщений.
 
 library(kernlab)
 library(kknn)
 
-data(spam)
+# data(spam)
+# 
+# idx <- sample(1:dim(spam)[1], 20)
+# 
+# spamtrain <- spam[-idx,]
+# spamtest <- spamtrain[idx,]
+# 
+# 
+# spamknnn <- kknn(type~., spamtrain, spamtest, distance = 1, kernel = "triangular")
+# summary(spamknnn)
+# 
+# fit <- fitted(spamknnn)
+# table(fit, spamtest$type)
 
-spamtrain <- spam[-idx,]
-spamtest <- spamtrain[idx,]
+xo <- read.table(file = "Tic_tac_toe.txt", sep = ",", stringsAsFactors = TRUE)
+
+idx <- sample(1:dim(xo)[1], 20)
+
+xotrain <- xo[-idx,]
+xotest  <- xo[idx,]
+
+xokknn <- kknn(V10~., xotrain, xotest, distance = 1, kernel = "triangular")
+summary(xokknn)
+
+fit <- fitted(xokknn)
+table(fit, xotest$V10)
