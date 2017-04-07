fcfb8d2325f1307a7835d44fabe80f5f83cc9b66
rch_9 <enlofpoliteh121@gmail.com>
2017-04-07 13:09:56 +0300

    added task1 and 2

--- lab3/example1.R
+++ lab3/example1.R
@@ -1,19 +1,33 @@
 library(tree)
-# 
-# data(iris)
-# 
-# ir.tr <- tree(Species ~., iris)
-# 
-# plot(ir.tr, type = "uniform")
-# text(ir.tr)
-
 library(maptree)
+
+data(iris)
+
+ir.tr <- tree(Species ~., iris)
+
+plot(ir.tr, type = "uniform")
+text(ir.tr)
+
 draw.tree(ir.tr)
 
-ir.tr
+print(ir.tr)
 
 ir.tr1 <- snip.tree(ir.tr, nodes = 12)
 draw.tree(ir.tr1)
 
+print(ir.tr1)
+
 ir.tr2 <- prune.tree(ir.tr, k = 10)
 draw.tree(ir.tr2)
+
+print(ir.tr2)
+
+
+par(pty = "s")
+
+
+plot(iris[, 3], iris[, 4], type = "n", xlab = "X1 petal length", ylab = "X2 petal width") 
+text(iris[, 3], iris[, 4], c("s", "c", "v")[iris[, 5]], col=c("blue", "red", "black")[iris[, 5]])
+partition.tree(ir.tr1, add = TRUE)
+
+
---
+++ lab3/task3.R
@@ -0,0 +1,23 @@
+# Загрузите набор данных nsw74psid1 из пакета DAAG. 
+# Постройте регресионное дерево для модели, задаваемой следующей 
+# формулой: re78 ~.. Постройте регресионную модель и 
+# SVM-регрессию для данной формулы. Сравните качество 
+# построенных моделей, выберите оптимальную и объясните свой выбор
+
+library(DAAG)
+library(tree)
+library(maptree)
+
+data(nsw74psid1)
+
+nsw74psid1[1,]
+
+nsw.tr <- tree(re78 ~., nsw74psid1)
+
+draw.tree(nsw.tr)
+print(nsw.tr)
+
+monica.tr1 <- snip.tree(monica.tr, nodes = 4)
+
+draw.tree(monica.tr1)
+print(monica.tr1)
