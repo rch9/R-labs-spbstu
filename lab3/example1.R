8a094e5ffba61f4f97bdff31dce63211565ef9fb
rch9 <enlofpoliteh121@gmail.com>
2017-04-09 16:37:25 +0300

    added fixes ci

--- lab3/example1.R
+++ lab3/example1.R
@@ -1,11 +1,11 @@
 library(tree)
 # 
-# data(iris)
-# 
-# ir.tr <- tree(Species ~., iris)
-# 
-# plot(ir.tr, type = "uniform")
-# text(ir.tr)
+data(iris)
+
+ir.tr <- tree(Species ~., iris)
+
+plot(ir.tr, type = "uniform")
+text(ir.tr)
 
 library(maptree)
 draw.tree(ir.tr)
@@ -17,3 +17,12 @@ draw.tree(ir.tr1)
 
 ir.tr2 <- prune.tree(ir.tr, k = 10)
 draw.tree(ir.tr2)
+
+
+par(pty = "s")
+
+plot(iris[, 3], iris[, 4], type = "n", xlab = "X1 petal length", ylab = "X2 petal width")
+
+text(iris[, 3], iris[, 4], c("s", "c", "v")[iris[, 5]], col=c("blue", "red", "black")[iris[, 5]])
+
+partition.tree(ir.tr1, add = TRUE)
