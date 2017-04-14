6ce406574c9722dc88c8989f557bcd9a50947156
rch9 <enlofpoliteh121@gmail.com>
2017-04-14 20:11:32 +0300

    add task1

---
+++ lab4/task1.R
@@ -0,0 +1,15 @@
+# Разбейте множество объектов из набора данных pluton в пакете «cluster» 
+# на 3 кластера методом центров тяжести (kmeans). Сравните качество 
+# разбиения в зависимости от максимального числа итераций алгоритма.
+
+library(cluster)
+
+data("pluton")
+
+for(i in 1:10)
+{
+  cl <- kmeans(pluton, 3, iter.max = i) 
+  plot(pluton, col = cl$cluster)
+  points(cl$centers, col = 1:2, pch = 8, cex=2)
+}
+
